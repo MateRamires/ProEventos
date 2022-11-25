eac2a5cbe09d7d5d4ecf05a49442428a03f4a488
MateRamires <matheushenriqueramires@gmail.com>
2022-11-24 22:25:34 -0300

    Form de perfil completo.

--- front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,3 +1,5 @@
+import { ValidatorField } from './../../../helpers/ValidatorField';
+import { FormGroup, FormBuilder, AbstractControlOptions, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
+    this.validation();
+  }
+
+  public validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    }
+
+    this.form = this.fb.group({
+      titulo:['', Validators.required],
+      primeiroNome:['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      telefone:['', Validators.required],
+      funcao:['', Validators.required],
+      descricao:['', Validators.required],
+      senha:(['', [Validators.required, Validators.minLength(6)]]),
+      confirmeSenha:['', Validators.required],
+    }, formOptions)
   }
 
 }
